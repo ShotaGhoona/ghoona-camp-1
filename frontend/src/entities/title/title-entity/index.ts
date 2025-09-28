1a071127ac00d9073707877190d21cbf16d426f0
ShotaGhoona <shota.yamashita@ghoona.com>
2025-09-28 23:52:44 +0900

    delete: past-code

--- frontend/src/entities/title/title-entity/index.ts
+++
@@ -1,9 +0,0 @@
-// Title Entity Exports
-// Export API functions
-// export * from './api';
-
-// Export models/types
-// export * from './model';
-
-// Export utilities/helpers
-// export * from './lib';
